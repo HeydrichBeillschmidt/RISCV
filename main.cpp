1b0efecd76905000fe9e0bfb609bb2a885f87413
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2020-07-16 09:20:51 +0800

    io adjusted

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include "pcpu.h"
 
-int main(int argc, const char* argv[]) {
-    const std::string path = argv[1];
-    ddl::CPU cpu(path);
+int main() {
+    ddl::CPU cpu;
     cpu.CC();
 
     return 0;
